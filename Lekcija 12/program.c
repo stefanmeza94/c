99f12724498d5a3c4907c62f1b79f28b8d3f328b
Stefan Meza <stefanmeza93@gmail.com>
2024-10-10 12:02:46 +0200

    Add more about functions

---
+++ Lekcija 12/program.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void prikaziPorukeDobrodoslice() {
+  printf("Dobro nam dosli!\n");
+  printf("Ovaj program konvertuje temperature iz *C u *F\n");
+  printf("Ispratite uputstva za rad programa\n");
+}
+
+float ucitajTemperaturuOdKorisnika() {
+  float temperatura;
+  printf("Unesite temperaturu u stepenima celzijusa: ");
+  scanf("%f", &temperatura);
+  return temperatura;
+}
+
+float konvertujTemperaturuCuF(float tempC) {
+  float farenhajt;
+  farenhajt = (1.8 * tempC) + 32;
+  return farenhajt;
+}
+
+void prikaziTemperaturu(float tempF) {
+  printf("Temperatura je %.2f*F\n", tempF);
+}
+
+void main() {
+  // float tempC, tempF;
+
+  // prikaziPorukeDobrodoslice();
+  // tempC = ucitajTemperaturuOdKorisnika();
+  // tempF = konvertujTemperaturuCuF(tempC);
+
+  // prikaziTemperaturu(tempF);
+
+  prikaziPorukeDobrodoslice();
+  prikaziTemperaturu(konvertujTemperaturuCuF(ucitajTemperaturuOdKorisnika()));
+}
